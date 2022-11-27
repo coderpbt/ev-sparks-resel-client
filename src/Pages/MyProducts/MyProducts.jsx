b0be2ca17951e6761c0431acc375ef3a60efcd0a
coderpbt <yeasin800398@gmail.com>
2022-11-27 23:45:59 +0600

    product delete

--- src/Pages/MyProducts/MyProducts.jsx
+++ src/Pages/MyProducts/MyProducts.jsx
@@ -1,18 +1,19 @@
 import { useQuery } from '@tanstack/react-query';
 import React, { useContext } from 'react';
 import { AuthContext } from '../../context/DpiContext/ContextProvider';
+import MyProductsTem from './MyProductsTem';
 
 const MyProducts = () => {
-    const {user} = useContext(AuthContext)
+    const { user } = useContext(AuthContext)
 
     const url = `http://localhost:5000/productswise?email=${user?.email}`
 
-    const {data : productswise = []} = useQuery({
-        queryKey : ['productswise', user?.email ],
-        queryFn : async () => {
+    const { data: productswise = [] } = useQuery({
+        queryKey: ['productswise', user?.email],
+        queryFn: async () => {
             const res = await fetch(url, {
-                headers : {
-                    authorization : `bearer ${localStorage.getItem('accessToken')}`
+                headers: {
+                    authorization: `bearer ${localStorage.getItem('accessToken')}`
                 }
             });
             const data = res.json()
@@ -20,35 +21,24 @@ const MyProducts = () => {
         }
 
     })
+
+    const handleDelete = () => {
+
+    }
+
     return (
         <div>
-        <h3 className="text-3xl mb-5">My Order</h3>
-        <div className="overflow-x-auto">
-            <table className="table w-full">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Name</th>
-                        <th>Product Name</th>
-                        <th>Time</th>  
-                        <th>Location</th>                 
-                        <th>Payment</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        productswise?.map((booking, i) => <tr key={booking._id}>
-                            <th>{i+1}</th>
-                            <td>{booking.email}</td>
-                            <td>{booking.name}</td>
-                            
-                           
-                        </tr>)
-                    }
-                </tbody>
-            </table>
+            <h3 className="text-3xl mb-5 mt-5">My Product</h3>
+            <div className='xl:w-[1200px] mx-auto w-[95%]'>
+                <div className="flex justify-between flex-wrap my-10">
+                    <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4 pt-2'>
+                        {
+                            productswise.map(item => <MyProductsTem item={item} key={item._id} handleDelete={handleDelete} />)
+                        }
+                    </div>
+                </div>
+            </div>
         </div>
-    </div>
     );
 };
 
--- src/Pages/MyProducts/MyProductsTem.jsx
+++ src/Pages/MyProducts/MyProductsTem.jsx
@@ -1,34 +1,28 @@
-import React from "react";
+import React from 'react';
 
-const CatagoryWiseProductTem = ({ item, setBook, }) => {
-  const { image, productName, location, reselPrice, originPrice, useOfYear, productCondition, yourName, date } = item;
-
+const MyProductsTem = ({item, handleDelete}) => {
+  const { image, productName, location, reselPrice, originPrice, productCondition, date } = item;
   return (
     <div>
-      <div className="card card-compact shadow-xl">
+            <div className="card card-compact shadow-xl">
         <figure>
           <img src={image} alt="logos" />
         </figure>
         <div className="card-body">
           <h2 className="card-title text-lg text-left text-black">{productName}</h2>
-          <div className="flex justify-between items-center">
-            <h2 className="card-title text-lg text-left text-black">post by <span className="text-blue-600 underline">{yourName}</span></h2>
-            <h3 className="text-base text-black text-left">Use : {useOfYear}</h3>
-          </div>
-
           <h3 className="text-base text-black text-left">location : {location}</h3>
           <h2 className="card-title text-base text-black">Orginal Price : ${reselPrice} </h2>
           <h2 className="card-title text-base text-black">Resale Price : ${originPrice}</h2>
           <h2 className="card-title text-base text-black">Product Condition: {productCondition}</h2>
           <h3 className="text-base text-black text-left">{date}</h3>
           <div className="card-actions justify-end">
-            <label
-              htmlFor="booking-modal"
-              onClick={() => setBook(item)}
+            <button
+              onClick={() => handleDelete(item)}
               className="btn btn-primary text-white"
             >
-              Book
-            </label>
+              Delete
+            </button>
+            <button className="btn btn-primary text-white">advertise</button>
           </div>
         </div>
       </div>
@@ -36,4 +30,4 @@ const CatagoryWiseProductTem = ({ item, setBook, }) => {
   );
 };
 
-export default CatagoryWiseProductTem;
+export default MyProductsTem;
